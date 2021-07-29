2341f3cec1a87763840131b09507340d5986b5fd
Puneeth C <puneethchoppanati@gmail.com>
2021-07-29 19:37:26 +0530

    Time: 52 ms (48.48%), Space: 15.1 MB (22.51%) - LeetHub

---
+++ search-insert-position/search-insert-position.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i = 0
+        while (i < len(nums) and nums[i] < target):
+            i += 1
+        return i
