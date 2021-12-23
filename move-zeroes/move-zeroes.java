35fe7f8d55afbecfcaea440afb709b634dea59ba
Puneeth C <puneethchoppanati@gmail.com>
2021-12-23 21:33:14 +0530

    Time: 1 ms (100.00%), Space: 39.9 MB (90.19%) - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
+       int i = 0;
         for(int j = 0; j < nums.length; j++){
             if(nums[j] != 0){
                 int temp = nums[i];
@@ -9,6 +9,5 @@ class Solution {
                 i++;
             }
         }
-        
     }
 }
