40b585506abfb2be5df60899699c7c9b4262828b
Puneeth C <puneethchoppanati@gmail.com>
2021-12-25 22:09:42 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (16.69%) - LeetHub

--- search-insert-position/search-insert-position.java
+++ search-insert-position/search-insert-position.java
@@ -1,16 +1,13 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while(left <= right){
-            int pivot = left + ((right - left)/2);
-            if(nums[pivot] == target){
-                return pivot;
-            } else if(nums[pivot] < target){
-                left = pivot + 1;
-            } else {
-                right = pivot - 1;
-            }
+        int l = 0, r = nums.length - 1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
         }
-        return left;
+        return l;
     }
 }
