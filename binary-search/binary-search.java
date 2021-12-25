7f2a9186516477dd42dd39392e73bae9f867bb24
Puneeth C <puneethchoppanati@gmail.com>
2021-12-25 21:45:44 +0530

    Time: 0 ms (100.00%), Space: 52 MB (5.16%) - LeetHub

--- binary-search/binary-search.java
+++ binary-search/binary-search.java
@@ -1,17 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l = nums.length;
-        if(l == 0)
-            return -1;
-        int left = 0, right = l - 1;
-        while(left <= right){
-            int pivot = (right+left)/2;
-            if(nums[pivot] == target)
-                return pivot;
-            else if(nums[pivot] < target)
-                left = pivot + 1;
+        int l = 0, r = nums.length - 1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                l = mid + 1;
             else
-                right = pivot - 1;
+                r = mid - 1;
         }
         return -1;
     }
