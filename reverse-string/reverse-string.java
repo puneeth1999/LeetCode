33fa3f9f1ebf61d32f6b25ca3b863d39a55d8221
Puneeth C <puneethchoppanati@gmail.com>
2021-12-29 21:05:49 +0530

    Time: 1 ms (94.58%), Space: 52.6 MB (8.88%) - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -2,10 +2,10 @@ class Solution {
     public void reverseString(char[] s) {
         int i = 0, j = s.length - 1;
         while(i < j){
-            // Swap
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
+            
             i++;
             j--;
         }
