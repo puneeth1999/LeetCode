3161466b181484905d91727d5cf2a18208c0bcb1
Puneeth C <puneethchoppanati@gmail.com>
2022-01-11 18:35:09 +0530

    Time: 14 ms, Memory: 38.9 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int i = 0, j = 0;
+        // if(arr.length == 1) return;
+        while(i< arr.length - 1){
+            if(arr[i] == 0){
+                int p = i+1;
+                j = i + 1;
+                int temp = arr[j];
+                while(j < arr.length - 1){
+                    int temp2 = arr[j+1];
+                    arr[j + 1] = temp;
+                    temp = temp2;
+                    j++;
+                }
+                if(p < arr.length)
+                    arr[p] = 0;
+                i += 2;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+}
