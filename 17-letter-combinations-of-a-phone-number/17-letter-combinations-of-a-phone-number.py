058122a3b1b287d88bd2a7875f9fa2550c4f0a1b
Puneeth C <puneethchoppanati@gmail.com>
2022-01-24 20:24:59 +0530

    Time: 32 ms (73.77%), Space: 14.2 MB (64.05%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if(len(digits) == 0):
+            return [] # return empty list
+        dic = {'2' : "abc", '3' : 'def', '4' : 'ghi', '5' : 'jkl', '6' : 'mno', '7' : 'pqrs', '8' : 'tuv', '9' : 'wxyz'};
+        res = [""]
+        for digit in digits:
+            val = dic[digit]
+            res = [x+y for x in res for y in val]
+        return res
