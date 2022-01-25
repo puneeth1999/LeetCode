c467e2e2d14379bc2088c929c557c8d627220bbf
Puneeth C <puneethchoppanati@gmail.com>
2022-01-26 00:03:07 +0530

    Time: 3 ms, Memory: 52.3 MB - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,13 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        for(int j = 0; j < nums.length; j++){
-            if(nums[j] != 0){
+        if(nums.length <=1)
+            return;
+        int j = 0;
+        while(j < nums.length && nums[j] != 0){
+            j++;
+        }
+        int i = j+1;
+        while(i < nums.length){
+            if(nums[i] != 0){
+                // swap
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                i++;
+                // increment j
+                j++;
             }
+            i++;
         }
     }
 }
