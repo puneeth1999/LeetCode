ed72f5c7ff0bd3ff0246c7054f7fafb0994dd5af
Puneeth C <puneethchoppanati@gmail.com>
2022-01-25 22:35:37 +0530

    Time: 1 ms, Memory: 56.2 MB - LeetHub

--- rotate-array/rotate-array.java
+++ rotate-array/rotate-array.java
@@ -1,28 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        /*
-         * Solution: reverse every integer in the array, reverse the kth section again
-         * and reverse the remaining section as well.
-         */
-        int len = nums.length;
-        k = k % len; // In case k > nums.length
-        // Reverse the whole array
-        nums = reverseArray(nums, 0, len - 1);
-        // Reverse the first slice of the array
-        nums = reverseArray(nums, 0, k-1);
-        // Reverse the remaining slice of the array
-        if (k < len - 1)
-            nums = reverseArray(nums, k, len - 1);
+        if(k == 0) return;
+        k = k % nums.length;
+        // Reverse the entire array
+        reverseRange(nums, 0, nums.length - 1);
+        // Reverse the first section
+        reverseRange(nums, 0, k - 1);
+        //Reverse the remaining section
+        reverseRange(nums, k, nums.length - 1);
     }
-    public static int[] reverseArray(int[] array, int start, int end) {
-        int i = start, j = end;
-        while (i < j) {
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            i++;
-            j--;
+    public void reverseRange(int[] nums, int i, int j){
+        while(i < j){
+            // swap
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
         }
-        return array;
     }
 }
