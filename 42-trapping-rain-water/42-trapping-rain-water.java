b3edcb8484310bdbd86cadfe3618bcee82ae3b7d
Puneeth C <puneethchoppanati@gmail.com>
2022-01-26 08:25:43 +0530

    Time: 2 ms (44.85%), Space: 42.3 MB (8.00%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] height) {
+        // Store the max_height on the left side of the current bar
+        int[] left_max = new int[height.length];
+        left_max[0] = height[0];
+        for(int i = 1; i < left_max.length; i++){
+            left_max[i] = Math.max(height[i], left_max[i - 1]);
+        }
+        
+        // Store the max_height on the right side of the current bar
+        int[] right_max = new int[height.length];
+        right_max[height.length - 1] = height[height.length - 1];
+        for(int i = height.length - 2; i>=0; i--){
+            right_max[i] = Math.max(height[i], right_max[i+1]);
+        }
+        
+        int ans = 0;
+        // find the #units of water that current bar can store with the help of left_max and right_max bar.
+        for(int i = 0; i < height.length; i++){
+            ans += (Math.min(right_max[i], left_max[i]) - height[i]);
+        }
+        
+        return ans;
+    }
+}
