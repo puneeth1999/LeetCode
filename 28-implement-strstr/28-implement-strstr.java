8bc32501ecf1556ba2a88ffe758c9234574df877
Puneeth C <puneethchoppanati@gmail.com>
2022-01-27 22:05:55 +0530

    Time: 1191 ms (28.55%), Space: 117.2 MB (15.89%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0)
+            return 0;
+        int needleLen = needle.length();
+        
+        char needlePoint = needle.charAt(0);
+        for(int i = 0; i < haystack.length(); i++){
+            if(haystack.charAt(i) == needlePoint){
+                if(i + needleLen > haystack.length())
+                    return -1;
+                String curr = haystack.substring(i, i+needleLen);
+                if(curr.equals(needle))
+                    return i;
+            }
+        }
+        return -1;
+    }
+}
