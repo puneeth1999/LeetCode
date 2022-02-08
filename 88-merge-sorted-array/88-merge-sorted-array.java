6b314e50d5fb384da1d309f491dc9479b086a2e0
Puneeth C <puneethchoppanati@gmail.com>
2022-02-08 10:39:08 +0530

    Time: 1 ms (28.54%), Space: 43.3 MB (5.69%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,26 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0, j = 0;
+        int i = 0; int j = 0;
         while(i < m && j < n){
-            if(nums1[i] >= nums2[j]){
-                shiftAndInsert(nums2[j], i, nums1);
-                j++;
-                m++;
+            if(nums2[j] < nums1[i]){
+                shiftAndInsert(nums1, nums2[j], i);
+                j++; m++;
             }
             i++;
         }
-        while(j < n){
+        while(i < nums1.length && j < n){
             nums1[i] = nums2[j];
             i++;
             j++;
         }
     }
-    public void shiftAndInsert(int a, int idx, int[] arr){
-        int x = arr.length - 1;
-        while(x > idx){
-            arr[x] = arr[x-1];
-            x--;
+    public void shiftAndInsert(int[] nums1, int insertElement, int pos){
+        int j = nums1.length - 1;
+        while(j > pos){
+            nums1[j] = nums1[j-1];
+            j--;
         }
-        arr[idx] = a;
-    }
+        nums1[pos] = insertElement;
+    }    
 }
