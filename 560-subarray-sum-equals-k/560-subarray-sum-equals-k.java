6e911270a1aad573dfe6604490b67848988312be
Puneeth C <puneethchoppanati@gmail.com>
2022-02-10 16:59:07 +0530

    Time: 1908 ms (5.85%), Space: 54.4 MB (51.51%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        // Using extra space
+        int ans = 0;
+        int[] adds = new int[nums.length + 1];
+        adds[0] = 0;
+        for(int i = 1; i <= nums.length; i++){
+            adds[i] = nums[i-1] + adds[i-1];
+        }
+        for(int i = 0; i < adds.length; i++){
+            for(int j = i+1; j < adds.length; j++){
+                if(adds[j] - adds[i] == k){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
