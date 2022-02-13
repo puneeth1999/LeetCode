e46b83bc1e157ddd155da0bd360f66f138511fad
Puneeth C <puneethchoppanati@gmail.com>
2022-02-13 17:06:31 +0530

    Time: 326 ms (5.01%), Space: 54.6 MB (5.00%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -7,11 +7,22 @@ class Solution {
             left_max[i] = Math.max(height[i], left_max[i - 1]);
         }
         
+        for(int i = 0; i < left_max.length; i++){
+            // int x = Math.max(height[i], left_max[i - 1]);
+            System.out.print(left_max[i] + " ");
+        }
+        
         // Store the max_height on the right side of the current bar
         int[] right_max = new int[height.length];
         right_max[height.length - 1] = height[height.length - 1];
         for(int i = height.length - 2; i>=0; i--){
             right_max[i] = Math.max(height[i], right_max[i+1]);
+        }
+        
+        System.out.println();
+        for(int i = 0; i < right_max.length; i++){
+            
+            System.out.print(right_max[i] + " ");
         }
         
         int ans = 0;
