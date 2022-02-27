fc05c42c52868f9f7678a5e18266c866e803fbce
Puneeth C <puneethchoppanati@gmail.com>
2022-02-27 15:46:40 +0530

    Added solution - LeetHub

---
+++ Rotate a Linked List - GFG/rotate-a-linked-list.java
@@ -0,0 +1,155 @@
+// { Driver Code Starts
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+            
+            for (int i=0; i<n-1; i++)
+            {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution ob = new Solution();
+            head = ob.rotate(head,k);
+            printList(head);
+        }
+    }
+    
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+// } Driver Code Ends
+
+
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution{
+    //Function to rotate a linked list.
+    public Node rotate(Node head, int k) {
+        // add code here
+        if(head == null)
+        {
+            return head;
+        }
+        // printList(head);
+        
+        int l = 0;
+        Node curr = head;
+        while(curr != null){
+            curr = curr.next;
+            l++;
+        }
+        if(l == k)
+            return head;
+        k = l - k + 1;
+        Node rev = reverse(head, null);
+        // printList(rev);
+        
+        int i = 1;
+        Node c1 = rev;
+        while(i < k){
+            i++;
+            c1 = c1.next;
+        }
+        
+        Node part1 = reverse(rev, c1);
+        // printList(part1);
+        
+        Node part2 = reverse(c1, null);
+        // printList(part2);
+        
+        c1 = part1;
+        while(c1.next != null){
+            c1 = c1.next;
+        }
+        c1.next = part2;
+        
+        return part1;
+        
+    }
+    
+    public Node reverse(Node curr, Node end){
+        Node prev = null;
+        Node next = null;
+        while(curr != end){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    // public void printList(Node NODE){
+    //     Node N = NODE;
+    //     while(N != null){
+    //         System.out.print(N.data + " ");
+    //         N = N.next;
+    //     }System.out.println();
+    // }
+    
+}
+
+// class Solution{
+//   //Function to rotate a linked list.
+//   public Node rotate(Node head, int k) {
+//       // add code here
+//       int n=0;
+//       Node out=null;
+//       Node curr=head;
+//       while(n<k-1){
+//           curr=curr.next;
+//           n++;
+//       }
+//       if(curr.next==null){
+//           return head;
+//       }
+//       Node temp=curr.next;
+//       out=temp;
+//       curr.next=null;
+//       while(temp.next!=null){
+//           temp=temp.next;
+//       }
+//       temp.next=head;
+       
+//       return out;
+//   }
+// }
