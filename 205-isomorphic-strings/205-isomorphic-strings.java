d578268c701465a0dfa4fb12658cbbf29742f9d1
Puneeth C <puneethchoppanati@gmail.com>
2022-06-19 21:26:48 +0530

    Time: 4 ms (95.69%), Space: 43.4 MB (25.90%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] map_s_t = new int[256];
-        int[] map_t_s = new int[256];
-        Arrays.fill(map_s_t, -1); Arrays.fill(map_t_s, -1);
+        int [] mapST = new int[256];
+        int [] mapTS = new int[256];
+        Arrays.fill(mapST, -1); Arrays.fill(mapTS, -1);
         for(int i = 0; i < s.length(); i++){
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-            if(map_s_t[c1] == -1 && map_t_s[c2] == -1){
-                map_s_t[c1] = c2;
-                map_t_s[c2] = c1;
-            } else if(map_s_t[c1] != c2 || map_t_s[c2] != c1){
+            // If both are untouched
+            if(mapST[c1] == -1 && mapTS[c2] == -1){
+                mapST[c1] = c2;
+                mapTS[c2] = c1;
+            } else if(mapST[c1] != c2 || mapTS[c2] != c1){
                 return false;
             }
         }
